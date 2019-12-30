7b29c723d9a8bcf6aded9143e27ca31ac0788a57
Joakim Koljonen <joakim.koljonen@trustly.com>
2019-12-30 21:50:50 +0100

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -30,11 +30,4 @@ function Loading(props) {
     return <div id={props.id}>{props.loading ? '✈' : ''}</div>;
 }
 
-function parseDuration(s) {
-    const parts = s.split(' ');
-    const numberPart = parts[0] === 'a' || parts[0] === 'an' ? 1 : parseInt(parts[0]);
-    const unitPart =  parts[1][parts[1].length -1] === 's' ? parts[1] : parts[1] + 's';
-    return moment.duration(numberPart, unitPart);
-}
-
 export default App;
