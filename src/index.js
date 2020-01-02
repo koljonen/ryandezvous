f4247773c59aeb08bdad0083ea6bc764313cc157
Joakim Koljonen <joakim.koljonen@trustly.com>
2020-01-02 23:13:21 +0100

    Switch to HashRouter
    BrowserRouter did not work at all on GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import './index.css'
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
+import { Switch, Route, HashRouter as Router } from 'react-router-dom'
 import App from './App'
 
 const routing = (
