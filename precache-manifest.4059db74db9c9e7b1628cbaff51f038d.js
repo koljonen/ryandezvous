045f008b1547d2b59a88bf0a5e3cad3891d41aa7
Joakim Koljonen <joakim.koljonen@trustly.com>
2020-01-06 21:20:46 +0100

    Updates

--- precache-manifest.4059db74db9c9e7b1628cbaff51f038d.js
+++ precache-manifest.4059db74db9c9e7b1628cbaff51f038d.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "421d4c08b58b5ed7d1db1136d28eb5c3",
+    "revision": "ec5d22ce97e95dfcdf4fb8cc8440f047",
     "url": "/ryandezvous/index.html"
   },
   {
-    "revision": "81c7592727e891b31de8",
+    "revision": "64ad5b637f02e6d7a380",
     "url": "/ryandezvous/static/css/2.07d92358.chunk.css"
   },
   {
-    "revision": "03d28f8adeb942aee504",
+    "revision": "d0fe09753a335b8ac26d",
     "url": "/ryandezvous/static/css/main.d321a45c.chunk.css"
   },
   {
-    "revision": "81c7592727e891b31de8",
-    "url": "/ryandezvous/static/js/2.2756d102.chunk.js"
+    "revision": "64ad5b637f02e6d7a380",
+    "url": "/ryandezvous/static/js/2.79e55cf9.chunk.js"
   },
   {
     "revision": "d171c94eb2a0ca4cd5654867670957df",
-    "url": "/ryandezvous/static/js/2.2756d102.chunk.js.LICENSE"
+    "url": "/ryandezvous/static/js/2.79e55cf9.chunk.js.LICENSE"
   },
   {
-    "revision": "03d28f8adeb942aee504",
-    "url": "/ryandezvous/static/js/main.1739be99.chunk.js"
+    "revision": "d0fe09753a335b8ac26d",
+    "url": "/ryandezvous/static/js/main.3e06fbf6.chunk.js"
   },
   {
     "revision": "56552e21418d808c75e5",
